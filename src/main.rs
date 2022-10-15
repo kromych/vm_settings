571bc1c09ac119a4bee09ccf4e1180ab62e17334
kromych <kromych@users.noreply.github.com>
2022-10-15 13:23:35 -0700

    Handle errors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use windows::core::BSTR;
-use windows::core::PCWSTR;
+use windows::core::HSTRING;
+use windows::Win32::Foundation::DISP_E_TYPEMISMATCH;
 use windows::Win32::Security::PSECURITY_DESCRIPTOR;
 use windows::Win32::System::Com::CoCreateInstance;
 use windows::Win32::System::Com::CoInitializeEx;
@@ -19,6 +20,7 @@ use windows::Win32::System::Wmi::IWbemClassObject;
 use windows::Win32::System::Wmi::IWbemLocator;
 use windows::Win32::System::Wmi::IWbemServices;
 use windows::Win32::System::Wmi::WbemLocator;
+use windows::Win32::System::Wmi::WBEM_E_NOT_FOUND;
 use windows::Win32::System::Wmi::WBEM_FLAG_FORWARD_ONLY;
 use windows::Win32::System::Wmi::WBEM_FLAG_RETURN_IMMEDIATELY;
 
@@ -58,17 +60,23 @@ fn connect_hyperv_wmi() -> Result<IWbemServices> {
     Ok(server)
 }
 
-fn get_row_item(o: &IWbemClassObject, name: &str) -> Result<String> {
+fn get_row_item(o: &IWbemClassObject, name: &str) -> Result<Option<String>> {
     let mut value: VARIANT = Default::default();
-    let wide_name = name.encode_utf16().collect::<Vec<_>>().as_ptr();
+    let name_hstr = HSTRING::from(name);
     unsafe {
-        o.Get(
-            PCWSTR(wide_name),
+        if let Err(e) = o.Get(
+            &name_hstr,
             0,
             &mut value,
             std::ptr::null_mut(),
             std::ptr::null_mut(),
-        )?;
+        ) {
+            if e.code().0 == WBEM_E_NOT_FOUND.0 {
+                return Ok(None);
+            } else {
+                anyhow::bail!(e);
+            }
+        }
 
         let bstr = VarFormat(
             &value,
@@ -76,10 +84,17 @@ fn get_row_item(o: &IWbemClassObject, name: &str) -> Result<String> {
             VARFORMAT_FIRST_DAY_SYSTEMDEFAULT,
             VARFORMAT_FIRST_WEEK_SYSTEMDEFAULT,
             0,
-        )?;
+        );
+        if let Err(e) = &bstr {
+            if e.code().0 == DISP_E_TYPEMISMATCH.0 {
+                return Ok(None);
+            } else {
+                anyhow::bail!(e.to_owned());
+            }
+        }
 
         VariantClear(&mut value)?;
-        Ok(String::from_utf16(bstr.as_wide())?)
+        Ok(Some(String::from_utf16(bstr?.as_wide())?))
     }
 }
 
@@ -91,41 +106,37 @@ trait WmiRowConstructable<T> {
 #[derive(Debug)]
 #[allow(dead_code)]
 struct HyperVVmSettings {
-    virtual_system_identifier: String,
-    configuration_data_root: String,
-    configuration_file: String,
-    firmware_file: String,
-    firmware_parameters: String,
-    guest_state_data_root: String,
-    guest_state_file: String,
-    guest_state_isolation_enabled: String,
-    guest_state_isolation_type: String,
-    is_saved: String,
-    virtual_system_sub_type: String,
-    secure_boot_enabled: String,
-    turn_off_on_guest_restart: String,
+    virtual_system_identifier: Option<String>,
+    configuration_data_root: Option<String>,
+    configuration_file: Option<String>,
+    firmware_file: Option<String>,
+    firmware_parameters: Option<String>,
+    guest_state_data_root: Option<String>,
+    guest_state_file: Option<String>,
+    guest_state_isolation_enabled: Option<String>,
+    guest_state_isolation_type: Option<String>,
+    is_saved: Option<String>,
+    virtual_system_sub_type: Option<String>,
+    secure_boot_enabled: Option<String>,
+    turn_off_on_guest_restart: Option<String>,
 }
 
 impl WmiRowConstructable<HyperVVmSettings> for HyperVVmSettings {
     fn from_row(row: &IWbemClassObject) -> Result<HyperVVmSettings> {
         Ok(HyperVVmSettings {
-            virtual_system_identifier: get_row_item(row, "VirtualSystemIdentifier")
-                .unwrap_or_default(),
-            configuration_data_root: get_row_item(row, "ConfigurationDataRoot").unwrap_or_default(),
-            configuration_file: get_row_item(row, "ConfigurationFile").unwrap_or_default(),
-            firmware_file: get_row_item(row, "FirmwareFile").unwrap_or_default(),
-            firmware_parameters: get_row_item(row, "FirmwareParameters").unwrap_or_default(),
-            guest_state_data_root: get_row_item(row, "GuestStateDataRoot").unwrap_or_default(),
-            guest_state_file: get_row_item(row, "GuestStateFile").unwrap_or_default(),
-            guest_state_isolation_enabled: get_row_item(row, "GuestStateIsolationEnabled")
-                .unwrap_or_default(),
-            guest_state_isolation_type: get_row_item(row, "GuestStateIsolationType")
-                .unwrap_or_default(),
-            is_saved: get_row_item(row, "IsSaved").unwrap_or_default(),
-            virtual_system_sub_type: get_row_item(row, "VirtualSystemSubType").unwrap_or_default(),
-            secure_boot_enabled: get_row_item(row, "SecureBootEnabled").unwrap_or_default(),
-            turn_off_on_guest_restart: get_row_item(row, "TurnOffOnGuestRestart")
-                .unwrap_or_default(),
+            virtual_system_identifier: get_row_item(row, "VirtualSystemIdentifier")?,
+            configuration_data_root: get_row_item(row, "ConfigurationDataRoot")?,
+            configuration_file: get_row_item(row, "ConfigurationFile")?,
+            firmware_file: get_row_item(row, "FirmwareFile")?,
+            firmware_parameters: get_row_item(row, "FirmwareParameters")?,
+            guest_state_data_root: get_row_item(row, "GuestStateDataRoot")?,
+            guest_state_file: get_row_item(row, "GuestStateFile")?,
+            guest_state_isolation_enabled: get_row_item(row, "GuestStateIsolationEnabled")?,
+            guest_state_isolation_type: get_row_item(row, "GuestStateIsolationType")?,
+            is_saved: get_row_item(row, "IsSaved")?,
+            virtual_system_sub_type: get_row_item(row, "VirtualSystemSubType")?,
+            secure_boot_enabled: get_row_item(row, "SecureBootEnabled")?,
+            turn_off_on_guest_restart: get_row_item(row, "TurnOffOnGuestRestart")?,
         })
     }
 
@@ -137,117 +148,106 @@ impl WmiRowConstructable<HyperVVmSettings> for HyperVVmSettings {
 #[derive(Debug)]
 #[allow(dead_code)]
 struct HyperVVmStatus {
-    available_requested_states: String,
-    caption: String,
-    communication_status: String,
-    creation_class_name: String,
-    dedicated: String,
-    description: String,
-    detailed_status: String,
-    element_name: String,
-    enabled_default: String,
-    enabled_state: String,
-    enhanced_session_mode_state: String,
-    failed_over_replication_type: String,
-    health_state: String,
-    hw_threads_per_core_realized: String,
-    identifying_descriptions: String,
-    install_date: String,
-    instance_id: String,
-    last_application_consistent_replication_time: String,
-    last_replication_time: String,
-    last_replication_type: String,
-    last_successful_backup_time: String,
-    name: String,
-    name_format: String,
-    number_of_numa_nodes: String,
-    on_time_in_milliseconds: String,
-    operating_status: String,
-    operational_status: String,
-    other_dedicated_descriptions: String,
-    other_enabled_state: String,
-    other_identifying_info: String,
-    power_management_capabilities: String,
-    primary_owner_contact: String,
-    primary_owner_name: String,
-    primary_status: String,
-    process_id: String,
-    replication_health: String,
-    replication_mode: String,
-    replication_state: String,
-    requested_state: String,
-    reset_capability: String,
-    roles: String,
-    status: String,
-    status_descriptions: String,
-    time_of_last_configuration_change: String,
-    time_of_last_state_change: String,
-    transitioning_to_state: String,
+    available_requested_states: Option<String>,
+    caption: Option<String>,
+    communication_status: Option<String>,
+    creation_class_name: Option<String>,
+    dedicated: Option<String>,
+    description: Option<String>,
+    detailed_status: Option<String>,
+    element_name: Option<String>,
+    enabled_default: Option<String>,
+    enabled_state: Option<String>,
+    enhanced_session_mode_state: Option<String>,
+    failed_over_replication_type: Option<String>,
+    health_state: Option<String>,
+    hw_threads_per_core_realized: Option<String>,
+    identifying_descriptions: Option<String>,
+    install_date: Option<String>,
+    instance_id: Option<String>,
+    last_application_consistent_replication_time: Option<String>,
+    last_replication_time: Option<String>,
+    last_replication_type: Option<String>,
+    last_successful_backup_time: Option<String>,
+    name: Option<String>,
+    name_format: Option<String>,
+    number_of_numa_nodes: Option<String>,
+    on_time_in_milliseconds: Option<String>,
+    operating_status: Option<String>,
+    operational_status: Option<String>,
+    other_dedicated_descriptions: Option<String>,
+    other_enabled_state: Option<String>,
+    other_identifying_info: Option<String>,
+    power_management_capabilities: Option<String>,
+    primary_owner_contact: Option<String>,
+    primary_owner_name: Option<String>,
+    primary_status: Option<String>,
+    process_id: Option<String>,
+    replication_health: Option<String>,
+    replication_mode: Option<String>,
+    replication_state: Option<String>,
+    requested_state: Option<String>,
+    reset_capability: Option<String>,
+    roles: Option<String>,
+    status: Option<String>,
+    status_descriptions: Option<String>,
+    time_of_last_configuration_change: Option<String>,
+    time_of_last_state_change: Option<String>,
+    transitioning_to_state: Option<String>,
 }
 
 impl WmiRowConstructable<HyperVVmStatus> for HyperVVmStatus {
     fn from_row(row: &IWbemClassObject) -> Result<HyperVVmStatus> {
         Ok(HyperVVmStatus {
-            available_requested_states: get_row_item(row, "AvailableRequestedStates")
-                .unwrap_or_default(),
-            caption: get_row_item(row, "Caption").unwrap_or_default(),
-            communication_status: get_row_item(row, "CommunicationStatus").unwrap_or_default(),
-            creation_class_name: get_row_item(row, "CreationClassName").unwrap_or_default(),
-            dedicated: get_row_item(row, "Dedicated").unwrap_or_default(),
-            description: get_row_item(row, "Description").unwrap_or_default(),
-            detailed_status: get_row_item(row, "DetailedStatus").unwrap_or_default(),
-            element_name: get_row_item(row, "ElementName").unwrap_or_default(),
-            enabled_default: get_row_item(row, "EnabledDefault").unwrap_or_default(),
-            enabled_state: get_row_item(row, "EnabledState").unwrap_or_default(),
-            enhanced_session_mode_state: get_row_item(row, "EnhancedSessionModeState")
-                .unwrap_or_default(),
-            failed_over_replication_type: get_row_item(row, "FailedOverReplicationType")
-                .unwrap_or_default(),
-            health_state: get_row_item(row, "HealthState").unwrap_or_default(),
-            hw_threads_per_core_realized: get_row_item(row, "HwThreadsPerCoreRealized")
-                .unwrap_or_default(),
-            identifying_descriptions: get_row_item(row, "IdentifyingDescriptions")
-                .unwrap_or_default(),
-            install_date: get_row_item(row, "InstallDate").unwrap_or_default(),
-            instance_id: get_row_item(row, "InstanceID").unwrap_or_default(),
+            available_requested_states: get_row_item(row, "AvailableRequestedStates")?,
+            caption: get_row_item(row, "Caption")?,
+            communication_status: get_row_item(row, "CommunicationStatus")?,
+            creation_class_name: get_row_item(row, "CreationClassName")?,
+            dedicated: get_row_item(row, "Dedicated")?,
+            description: get_row_item(row, "Description")?,
+            detailed_status: get_row_item(row, "DetailedStatus")?,
+            element_name: get_row_item(row, "ElementName")?,
+            enabled_default: get_row_item(row, "EnabledDefault")?,
+            enabled_state: get_row_item(row, "EnabledState")?,
+            enhanced_session_mode_state: get_row_item(row, "EnhancedSessionModeState")?,
+            failed_over_replication_type: get_row_item(row, "FailedOverReplicationType")?,
+            health_state: get_row_item(row, "HealthState")?,
+            hw_threads_per_core_realized: get_row_item(row, "HwThreadsPerCoreRealized")?,
+            identifying_descriptions: get_row_item(row, "IdentifyingDescriptions")?,
+            install_date: get_row_item(row, "InstallDate")?,
+            instance_id: get_row_item(row, "InstanceID")?,
             last_application_consistent_replication_time: get_row_item(
                 row,
                 "LastApplicationConsistentReplicationTime",
-            )
-            .unwrap_or_default(),
-            last_replication_time: get_row_item(row, "LastReplicationTime").unwrap_or_default(),
-            last_replication_type: get_row_item(row, "LastReplicationType").unwrap_or_default(),
-            last_successful_backup_time: get_row_item(row, "LastSuccessfulBackupTime")
-                .unwrap_or_default(),
-            name: get_row_item(row, "Name").unwrap_or_default(),
-            name_format: get_row_item(row, "NameFormat").unwrap_or_default(),
-            number_of_numa_nodes: get_row_item(row, "NumberOfNumaNodes").unwrap_or_default(),
-            on_time_in_milliseconds: get_row_item(row, "OnTimeInMilliseconds").unwrap_or_default(),
-            operating_status: get_row_item(row, "OperatingStatus").unwrap_or_default(),
-            operational_status: get_row_item(row, "OperationalStatus").unwrap_or_default(),
-            other_dedicated_descriptions: get_row_item(row, "OtherDedicatedDescriptions")
-                .unwrap_or_default(),
-            other_enabled_state: get_row_item(row, "OtherEnabledState").unwrap_or_default(),
-            other_identifying_info: get_row_item(row, "OtherIdentifyingInfo").unwrap_or_default(),
-            power_management_capabilities: get_row_item(row, "PowerManagementCapabilities")
-                .unwrap_or_default(),
-            primary_owner_contact: get_row_item(row, "PrimaryOwnerContact").unwrap_or_default(),
-            primary_owner_name: get_row_item(row, "PrimaryOwnerName").unwrap_or_default(),
-            primary_status: get_row_item(row, "PrimaryStatus").unwrap_or_default(),
-            process_id: get_row_item(row, "ProcessID").unwrap_or_default(),
-            replication_health: get_row_item(row, "ReplicationHealth").unwrap_or_default(),
-            replication_mode: get_row_item(row, "ReplicationMode").unwrap_or_default(),
-            replication_state: get_row_item(row, "ReplicationState").unwrap_or_default(),
-            requested_state: get_row_item(row, "RequestedState").unwrap_or_default(),
-            reset_capability: get_row_item(row, "ResetCapability").unwrap_or_default(),
-            roles: get_row_item(row, "Roles").unwrap_or_default(),
-            status: get_row_item(row, "Status").unwrap_or_default(),
-            status_descriptions: get_row_item(row, "StatusDescriptions").unwrap_or_default(),
-            time_of_last_configuration_change: get_row_item(row, "TimeOfLastConfigurationChange")
-                .unwrap_or_default(),
-            time_of_last_state_change: get_row_item(row, "TimeOfLastStateChange")
-                .unwrap_or_default(),
-            transitioning_to_state: get_row_item(row, "TransitioningToState").unwrap_or_default(),
+            )?,
+            last_replication_time: get_row_item(row, "LastReplicationTime")?,
+            last_replication_type: get_row_item(row, "LastReplicationType")?,
+            last_successful_backup_time: get_row_item(row, "LastSuccessfulBackupTime")?,
+            name: get_row_item(row, "Name")?,
+            name_format: get_row_item(row, "NameFormat")?,
+            number_of_numa_nodes: get_row_item(row, "NumberOfNumaNodes")?,
+            on_time_in_milliseconds: get_row_item(row, "OnTimeInMilliseconds")?,
+            operating_status: get_row_item(row, "OperatingStatus")?,
+            operational_status: get_row_item(row, "OperationalStatus")?,
+            other_dedicated_descriptions: get_row_item(row, "OtherDedicatedDescriptions")?,
+            other_enabled_state: get_row_item(row, "OtherEnabledState")?,
+            other_identifying_info: get_row_item(row, "OtherIdentifyingInfo")?,
+            power_management_capabilities: get_row_item(row, "PowerManagementCapabilities")?,
+            primary_owner_contact: get_row_item(row, "PrimaryOwnerContact")?,
+            primary_owner_name: get_row_item(row, "PrimaryOwnerName")?,
+            primary_status: get_row_item(row, "PrimaryStatus")?,
+            process_id: get_row_item(row, "ProcessID")?,
+            replication_health: get_row_item(row, "ReplicationHealth")?,
+            replication_mode: get_row_item(row, "ReplicationMode")?,
+            replication_state: get_row_item(row, "ReplicationState")?,
+            requested_state: get_row_item(row, "RequestedState")?,
+            reset_capability: get_row_item(row, "ResetCapability")?,
+            roles: get_row_item(row, "Roles")?,
+            status: get_row_item(row, "Status")?,
+            status_descriptions: get_row_item(row, "StatusDescriptions")?,
+            time_of_last_configuration_change: get_row_item(row, "TimeOfLastConfigurationChange")?,
+            time_of_last_state_change: get_row_item(row, "TimeOfLastStateChange")?,
+            transitioning_to_state: get_row_item(row, "TransitioningToState")?,
         })
     }
 
@@ -285,11 +285,11 @@ fn main() -> Result<()> {
     init_com()?;
 
     let server = connect_hyperv_wmi()?;
-    let status = query_one::<HyperVVmStatus>(&server, "alpine")?;
-    println!("{status:#?}");
-
-    let settings = query_one::<HyperVVmSettings>(&server, "alpine")?;
+    let settings = query_one::<HyperVVmSettings>(&server, "pico-linux")?;
     println!("{settings:#?}");
+
+    let status = query_one::<HyperVVmStatus>(&server, "pico-linux")?;
+    println!("{status:#?}");
 
     Ok(())
 }
